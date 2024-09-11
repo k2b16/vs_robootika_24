e0354693d094996ab28b71076762a82838ed643e
agnesrohtsalu <142823406+agnesrohtsalu@users.noreply.github.com>
2024-09-11 21:22:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 
 def main_loop():
     debug = True  
-    robot_motion = motion.OmniMotionRobot(port="/dev/ttyUSB0", baudrate=115200)
+    robot_motion = motion.OmniMotionRobot(port="/dev/ttyACM0")
     cam = camera.RealsenseCamera(exposure = 100)
     
     processor = image_processor.ImageProcessor(cam, debug=debug)
@@ -19,15 +19,16 @@ def main_loop():
     frame = 0
     frame_cnt = 0
     try:
-        robot_motion.move(0, 0, 20)
-        time.sleep(2)
         
-        robot_motion.move(0, 0, -20)
-        time.sleep(2)
+        #time.sleep(2)
         
-        robot_motion.move(0, 0, 0)
+        #robot_motion.move(0, 0, 0, -100)
+        #time.sleep(2)
+        
+        #robot_motion.move(0, 0, 0, 0)
 
         while True:
+            robot_motion.move(0, 0, 0, 100)
             processedData = processor.process_frame(aligned_depth=False)
 
             frame_cnt +=1
